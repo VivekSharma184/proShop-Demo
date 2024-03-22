acbffd6e71b9d3ab37097b117104c63f2c073be6
VivekSharma184 <vivicosharma@gmail.com>
2024-03-22 17:06:32 +0530

    Implement React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import { Container } from "react-bootstrap"
-import Header from "./Components/Header"
-import Footer from "./Components/Footer"
-import HomeScreen from "./Screens/HomeScreen"
-
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-
     <>
-    <Header/>
-    <main className="py-3">
-      <Container>
-    <h1><HomeScreen/></h1>
-    </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,20 +1,19 @@
-import { Container, Row, Col} from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-
-    const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
       <Container>
         <Row>
-            <Col className="text-Center py-3">
-                <p>ProShop &#169; {currentYear}</p>
-            </Col>
+          <Col className="text-Center py-3">
+            <p>ProShop &#169; {currentYear}</p>
+          </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
